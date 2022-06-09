2b4dbf9687f793c8c7704a7cb011f0f03f2782dd
seoilgun <seoilgun@gmail.com>
2022-06-09 14:43:08 +0900

    added train config

--- maskrcnn_benchmark/data/datasets/coco.py
+++ maskrcnn_benchmark/data/datasets/coco.py
@@ -101,10 +101,10 @@ class COCODataset(torchvision.datasets.coco.CocoDetection):
         masks = SegmentationMask(masks, img.size, mode='poly')
         target.add_field("masks", masks)
 
-        if anno and "keypoints" in anno[0]:
-            keypoints = [obj["keypoints"] for obj in anno]
-            keypoints = PersonKeypoints(keypoints, img.size)
-            target.add_field("keypoints", keypoints)
+        # if anno and "keypoints" in anno[0]:
+        #     keypoints = [obj["keypoints"] for obj in anno]
+        #     keypoints = PersonKeypoints(keypoints, img.size)
+        #     target.add_field("keypoints", keypoints)
 
         target = target.clip_to_image(remove_empty=True)
 
